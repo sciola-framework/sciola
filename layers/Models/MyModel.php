c6963119f4ec38ba3feb4be3ccd11ea2a60ac5ef
Sciola <115746943+sciola-git@users.noreply.github.com>
2022-10-26 11:00:03 -0300

    Create MyModel.php

---
+++ layers/Models/MyModel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Layers\Models;
+
+use Framework\Model;
+use Framework\Connection;
+use \PDO;
+
+class MyModel extends Model
+{
+    //private $pdo = null;
+    //private $orm = null;
+
+    public function __construct()
+    {
+        //$this->pdo = Connection::pdo();
+        //$this->orm = Connection::orm();
+    }
+}
