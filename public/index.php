6de835b6dcc23d9e7bb4c2125bc581b63a03b5d2
Sciola <115746943+sciola-framework@users.noreply.github.com>
2022-11-03 16:26:27 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@
     $autoload = "$dirname/packages/vendor/autoload.php";
     if (file_exists($autoload)) {
         include_once $autoload;
-        Framework\Settings::init($dirname);
+        Sciola\Settings::init($dirname);
     } else {
         echo 'Error: The autoload.php file was not found!';
     }
