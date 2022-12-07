68edd99166c1356a40535dc2225016c7584a0247
Leandro Sciola <118067473+Leandro-Sciola@users.noreply.github.com>
2022-12-06 23:13:19 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,9 @@
 <?php
 
 try {
+    if (is_file(__DIR__ . $_SERVER['REQUEST_URI'])) {
+        return false;
+    }
     $dirname  = dirname(__DIR__);
     $autoload = "$dirname/packages/node_modules/sciola/vendor/autoload.php";
     if (file_exists($autoload)) {
