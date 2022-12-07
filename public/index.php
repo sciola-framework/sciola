609d1007033409230d588849713779827c3e4bcd
Leandro Sciola <118067473+Leandro-Sciola@users.noreply.github.com>
2022-12-07 16:41:23 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,16 +1,5 @@
 <?php
 
-try {
-    if (is_file(__DIR__ . $_SERVER['REQUEST_URI'])) {
-        return false;
-    }
-    $dirname  = dirname(__DIR__);
-    $autoload = "$dirname/packages/node_modules/sciola/vendor/autoload.php";
-    if (file_exists($autoload)) {
-        include_once $autoload;
-        return Sciola\Settings::init($dirname);
-    }
-    throw new Exception('Error: The autoload.php file was not found!');
-} catch (Exception $e) {
-   echo $e->getMessage();
-}
+if (is_file(__DIR__ . $_SERVER['REQUEST_URI'])) return false;
+include_once dirname(__DIR__) . '/packages/node_modules/sciola/index.php';
+Sciola::init();
